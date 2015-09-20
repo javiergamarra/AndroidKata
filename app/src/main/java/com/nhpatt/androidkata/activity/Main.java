d6840b64b55572f9a2fa2d05aeb19f57b3878986
Javier Gamarra <nhpatt@gmail.com>
2015-09-20 19:04:07 +0200

    remove useless menu

--- app/src/main/java/com/nhpatt/androidkata/activity/Main.java
+++ app/src/main/java/com/nhpatt/androidkata/activity/Main.java
@@ -35,26 +35,6 @@ public class Main extends Activity implements View.OnClickListener {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
     public void onClick(View view) {
         //Getting view ID to know what button was pressed.
         switch (view.getId()) {
